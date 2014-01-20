5963a9b5cc06c599579c1dc6e0948760cffe8280
Luís Fernandes <luisapidcloud@hotmail.com>
2014-01-20 16:20:35 +0000

    vs fixes
    vs fixes

--- Gibbo.Editor.WPF/Controls/VisualScripting/VisualScriptingNode.xaml.cs
+++ Gibbo.Editor.WPF/Controls/VisualScripting/VisualScriptingNode.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Gibbo.Editor.WPF.Controls.VisualScripting
+namespace Gibbo.Editor.WPF
 {
     /// <summary>
     /// Interaction logic for VisualScriptingNode.xaml
