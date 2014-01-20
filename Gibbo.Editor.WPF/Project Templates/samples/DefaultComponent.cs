2f4ee0f78133d797dbf480969c0c496f6b065228
João Alves <joao.cpp.sca@gmail.com>
2014-01-20 16:00:05 +0000

    merge

--- Gibbo.Editor.WPF/Project Templates/samples/DefaultComponent.cs
+++ Gibbo.Editor.WPF/Project Templates/samples/DefaultComponent.cs
@@ -1,26 +1,4 @@
-﻿#region Copyrights
-/*
-Gibbo2D License - Version 1.0
-Copyright (c) 2013 - Gibbo2D Team
-Founders Joao Alves <joao.cpp.sca@gmail.com> & Luis Fernandes <luisapidcloud@hotmail.com>
-
-Permission is granted to use this software and associated documentation files (the "Software") free of charge, 
-to any person or company. The code can be used, modified and merged without restrictions, but you cannot sell 
-the software itself and parts where this license applies. Still, permission is granted for anyone to sell 
-applications made using this software (for example, a game). This software cannot be claimed as your own, 
-except for copyright holders. This license notes should also be available on any of the changed or added files.
-
-The software is provided "as is", without warranty of any kind, express or implied, including but not limited to 
-the warranties of merchantability, fitness for a particular purpose and non-infrigement. In no event shall the 
-authors or copyright holders be liable for any claim, damages or other liability.
-
-The license applies to all versions of the software, both newer and older than the one listed, unless a newer copy 
-of the license is available, in which case the most recent copy of the license supercedes all others.
-
-*/
-#endregion
-
-using System;
+﻿using System;
 using Gibbo.Library;
 using Microsoft.Xna.Framework;
 
